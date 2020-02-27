f91ba312ffbc0efe34fabca9ef599cb5c9af1c59
Haris Munir <Haris.Munir@qa.com>
2020-02-27 15:24:10 +0000

    Flask routes tutorial completed

--- application/routes.py
+++ application/routes.py
@@ -1,10 +1,23 @@
 from flask import render_template, url_for
 from application import app
 
+blogData = [
+        {
+            "name": {"first":"John", "last":"Doe"},
+            "title":"First Post",
+            "content":"This is some blog data for Flask lectures"
+        },
+        {   
+            "name": {"first":"Jane", "last":"Doe"},
+            "title":"Second Post",
+            "content":"This is even more blog data for Flask lectures"
+        }
+]
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title='Home')
+    return render_template('home.html', title='Home', posts=blogData)
 
 @app.route('/about')
 def about():
