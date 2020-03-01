6ccb4cf4093b5ad6f1457b9fa0b9fcc75bf9f7a1
Haris Munir <Haris.Munir@qa.com>
2020-03-01 16:08:30 +0000

    Form back end

--- application/__init__.py
+++ application/__init__.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
 db = SQLAlchemy(app)
 
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, url_for
 from application import app
 from application.models import Post
+from application.forms import PostForm
 
 @app.route('/')
 @app.route('/home')
@@ -11,6 +12,11 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/post/create')
+def post_create():
+    form = PostForm()
+    return render_template('post.html', form=form)
 
 @app.route('/register')
 def register():
