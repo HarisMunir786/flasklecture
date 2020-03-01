c6ad388e1ea4dada9a1b0b6914a4469b204299af
Haris Munir <Haris.Munir@qa.com>
2020-03-01 14:20:50 +0000

    DB connected and entries performed

--- application/routes.py
+++ application/routes.py
@@ -1,23 +1,13 @@
 from flask import render_template, url_for
 from application import app
+from application.models import Posts
 
-blogData = [
-        {
-            "name": {"first":"John", "last":"Doe"},
-            "title":"First Post",
-            "content":"This is some blog data for Flask lectures"
-        },
-        {   
-            "name": {"first":"Jane", "last":"Doe"},
-            "title":"Second Post",
-            "content":"This is even more blog data for Flask lectures"
-        }
-]
+data = []
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title='Home', posts=blogData)
+    return render_template('home.html', title='Home', posts=data)
 
 @app.route('/about')
 def about():
