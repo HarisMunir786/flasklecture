77bea5472b3165607f6b0c93807e057d69c82ee5
Haris Munir <Haris.Munir@qa.com>
2020-03-01 19:21:06 +0000

    postform connected with DB

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template, url_for
-from application import app
+from flask import render_template, url_for, redirect
+from application import app, db
 from application.models import Post
 from application.forms import PostForm
 
@@ -13,9 +13,19 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
-@app.route('/post/create')
+@app.route('/post/create', methods=['GET','POST'])
 def post_create():
     form = PostForm()
+    if form.validate_on_submit():
+        post = Post(
+            first_name = form.first_name.data,
+            last_name = form.last_name.data,
+            title = form.title.data,
+            content = form.content.data
+        )
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('home'))
     return render_template('post.html', form=form)
 
 @app.route('/register')
